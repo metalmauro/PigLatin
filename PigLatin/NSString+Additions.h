92f6e9b815f70cadc6b423964fd65ffb8095c34d
Matt Mauro <matt.mauro@gmail.com>
2016-11-04 14:56:44 -0400

    first commit

---
+++ PigLatin/NSString+Additions.h
@@ -0,0 +1,13 @@
+//
+//  NSString+Additions.h
+//  PigLatin
+//
+//  Created by Matthew Mauro on 2016-11-04.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (Additions)
+
+@end
---
+++ PigLatin/NSString+Additions.m
@@ -0,0 +1,13 @@
+//
+//  NSString+Additions.m
+//  PigLatin
+//
+//  Created by Matthew Mauro on 2016-11-04.
+//  Copyright © 2016 Matthew Mauro. All rights reserved.
+//
+
+#import "NSString+Additions.h"
+
+@implementation NSString (Additions)
+
+@end
